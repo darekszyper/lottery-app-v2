2faf7950eb2f9d66fb3929801155567cb1ae5deb
darekszyper <114878453+darekszyper@users.noreply.github.com>
2023-11-26 19:51:50 +0100

    Now participants can register for specific lottery with the same email as long as this specific email is not confirmed

---
+++ target/generated-sources/annotations/com/internship/juglottery/mapper/AppUserMapperImpl.java
@@ -0,0 +1,51 @@
+package com.internship.juglottery.mapper;
+
+import com.internship.juglottery.dto.request.AppUserRequest;
+import com.internship.juglottery.dto.response.AppUserResponse;
+import com.internship.juglottery.entity.AppUser;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-26T19:50:41+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.3 (JetBrains s.r.o.)"
+)
+@Component
+public class AppUserMapperImpl extends AppUserMapper {
+
+    @Override
+    public AppUserResponse toResponse(AppUser appUser) {
+        if ( appUser == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String email = null;
+        String name = null;
+
+        id = appUser.getId();
+        email = appUser.getEmail();
+        name = appUser.getName();
+
+        AppUserResponse appUserResponse = new AppUserResponse( id, email, name );
+
+        return appUserResponse;
+    }
+
+    @Override
+    public AppUser toEntity(AppUserRequest appUserRequest) {
+        if ( appUserRequest == null ) {
+            return null;
+        }
+
+        AppUser appUser = new AppUser();
+
+        setUserRoleAndEncodePassword( appUser );
+
+        appUser.setEmail( appUserRequest.getEmail() );
+        appUser.setName( appUserRequest.getName() );
+
+        return appUser;
+    }
+}
