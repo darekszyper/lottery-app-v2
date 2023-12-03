8e6c6e8595ff6af70b46f452f3ad77397aa7339a
darekszyper <114878453+darekszyper@users.noreply.github.com>
2023-12-03 16:29:01 +0100

    Events are allowed now to create only one background process for all tasks.

--- src/main/java/com/internship/juglottery/config/AsynchronousSpringEventsConfig.java
+++ src/main/java/com/internship/juglottery/config/AsynchronousSpringEventsConfig.java
@@ -1,19 +1,25 @@
 package com.internship.juglottery.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ApplicationEventMulticaster;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+        import org.springframework.context.event.ApplicationEventMulticaster;
+        import org.springframework.context.event.SimpleApplicationEventMulticaster;
 
 @Configuration
 public class AsynchronousSpringEventsConfig {
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
-        SimpleApplicationEventMulticaster eventMulticaster =
-                new SimpleApplicationEventMulticaster();
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
 
-        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(1);
+        taskExecutor.setMaxPoolSize(1);
+        taskExecutor.setQueueCapacity(1000);
+        taskExecutor.initialize();
+
+        eventMulticaster.setTaskExecutor(taskExecutor);
         return eventMulticaster;
     }
 }
+
--- src/test/java/com/internship/juglottery/service/impl/EmailSenderServiceTest.java
+++ src/test/java/com/internship/juglottery/service/impl/EmailSenderServiceTest.java
@@ -5,12 +5,15 @@ import com.internship.juglottery.entity.Participant;
 import com.internship.juglottery.entity.Voucher;
 import com.internship.juglottery.entity.Winner;
 import com.internship.juglottery.repository.WinnerRepo;
+import com.internship.juglottery.service.ParticipantService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
